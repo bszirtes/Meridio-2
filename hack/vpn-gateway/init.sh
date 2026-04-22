9eb4053d53d7105eaebf7063ec451a69233a83fb
Botond Szirtes <botond.szirtes@est.tech>
2026-04-22 11:41:25 +0200

    hack: add SCTP multihoming VLANs to vpn-gateway
    - Add VLAN 500/600 for sctp-multihoming test suite
    - Configure BGP peers GW4_SCTP1 (ASN 64516) and GW4_SCTP2 (ASN 64517)
    - Update comments to reflect actual suite names (separate-appnetwork, common-appnetwork)
    - Client VIPs: 200.100.0.100 (vlan5) and 200.100.1.100 (vlan6) for dual-path SCTP
    
    Signed-off-by: Botond Szirtes <botond.szirtes@est.tech>

--- hack/vpn-gateway/init.sh
+++ hack/vpn-gateway/init.sh
@@ -3,32 +3,44 @@
 sysctl -w net.ipv4.fib_multipath_hash_policy=1
 sysctl -w net.ipv4.conf.all.forwarding=1
 
-# VLAN 100 — ns-a gw-a1
+# VLAN 100 — separate-appnetwork gw-a1
 ip link add link eth0 name vlan1 type vlan id 100
 ip link set vlan1 up
 ip addr add 169.254.100.150/24 dev vlan1
 ip addr add 200.100.0.100/32 dev vlan1
 
-# VLAN 200 — ns-a gw-a2
+# VLAN 200 — separate-appnetwork gw-a2
 ip link add link eth0 name vlan2 type vlan id 200
 ip link set vlan2 up
 ip addr add 169.254.200.150/24 dev vlan2
 ip addr add 200.200.0.100/32 dev vlan2
 
-# VLAN 300 — ns-b gw-b1
+# VLAN 300 — common-appnetwork gw-b1
 ip link add link eth0 name vlan3 type vlan id 300
 ip link set vlan3 up
 ip addr add 169.254.101.150/24 dev vlan3
 ip addr add 200.100.0.101/32 dev vlan3
 
-# VLAN 400 — ns-b gw-b2
+# VLAN 400 — common-appnetwork gw-b2
 ip link add link eth0 name vlan4 type vlan id 400
 ip link set vlan4 up
 ip addr add 169.254.201.150/24 dev vlan4
 ip addr add 200.200.0.101/32 dev vlan4
 
+# VLAN 500 — sctp-multihoming path 1
+ip link add link eth0 name vlan5 type vlan id 500
+ip link set vlan5 up
+ip addr add 169.254.100.151/24 dev vlan5
+ip addr add 200.100.0.100/32 dev vlan5
+
+# VLAN 600 — sctp-multihoming path 2
+ip link add link eth0 name vlan6 type vlan id 600
+ip link set vlan6 up
+ip addr add 169.254.200.151/24 dev vlan6
+ip addr add 200.100.1.100/32 dev vlan6
+
 ethtool -K eth0 tx off
 
-echo "VPN Gateway ready on VLAN 100, 200, 300, 400"
+echo "VPN Gateway ready on VLAN 100, 200, 300, 400, 500, 600"
 
 /usr/sbin/bird -d -c /etc/bird/bird-gw.conf
